16587506fd2bbeddde8ab4af1cc76a9fece976a3
Eddie Stockton <130129190+imtrollmastr@users.noreply.github.com>
2024-02-21 09:47:21 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -21,11 +21,35 @@ input.onButtonPressed(Button.A, function () {
         usingpass = false
     }
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (shutdown == false) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        usingpass = false
+        shutdown = true
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        shutdown = false
+    }
+})
 let usingpass = false
+let shutdown = false
 let timesInBathroom = 0
 let user = ""
 user = "hugo"
 timesInBathroom = 0
+shutdown = false
 basic.showLeds(`
     # . . . #
     . # . # .
